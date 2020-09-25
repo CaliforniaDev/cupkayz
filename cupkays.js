bbe68005c4d3611c1a27adceae6db53d044c176a
Leo Daniels <leo@nybblebox.com>
2020-09-25 02:59:53 -0700

    created an event handler (init) to handle the onload event. also added new ID's to the navbar so manipulate the content of in the DOM

--- cupkays.js
+++ cupkays.js
@@ -1,38 +1,41 @@
 
 
-window.onload = function() {
+window.onload = init;
 
+function init() {
+  let navbar = document.getElementById("navbar");
+  let sticky = navbar.offsetTop;
+  let navbarContent = document.getElementById("navbar-content")
 
-window.onscroll = () => myFunction();
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
+window.onscroll = () => stickyNavbar();
+let stickyNavbar = () => {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
-  }
+  };
 }
-function displayWindowSize() {
 
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky");
-    } else {
-      navbar.classList.remove("sticky");
-    }
+function addCurtainOverlay() {
   let w = document.documentElement.clientWidth;
   let h = document.documentElement.clientHeight;
+  if (w <= 600) {
+    navbar.classList.add("overlay");
+    navbarContent.classList.add("overlay-content");
+  } else {
+    navbar.classList.remove("overlay");
+    navbarContent.classList.remove("overlay-content");
+  };
 
-  document.getElementById("result").innerHTML = "Width: " +
-                                        w + ", " + "Height: " + h;
 }
+// adds or removes navbar curtain as window resizes
+window.addEventListener("resize", addCurtainOverlay);
 
-window.addEventListener("resize", displayWindowSize);
-
+// end of init
+}
 
 
 
 
 
 //end of onload
-};

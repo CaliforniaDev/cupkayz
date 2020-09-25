6f261131cb5d2d3acb67f96cc2e427c55c6b566e
Leo Daniels <leo@nybblebox.com>
2020-09-25 03:58:43 -0700

    created a curtain navbar for displays 600px and less. need to created a new button for the curtain navbar. Bug: transition occurs as you resize the page.

--- cupkays.js
+++ cupkays.js
@@ -5,7 +5,9 @@ window.onload = init;
 function init() {
   let navbar = document.getElementById("navbar");
   let sticky = navbar.offsetTop;
-  let navbarContent = document.getElementById("navbar-content")
+  let navbarContent = document.getElementById("navbar-content");
+  let closeButton =
+  document.getElementById("closeBtn");
 
 window.onscroll = () => stickyNavbar();
 let stickyNavbar = () => {
@@ -14,24 +16,38 @@ let stickyNavbar = () => {
   } else {
     navbar.classList.remove("sticky");
   };
-}
+};
+// adds class as soon as the page loads
+navbar.classList.add("overlay");
+navbarContent.classList.add("overlay-content");
+// adds class as soon as the page loads
 
 function addCurtainOverlay() {
   let w = document.documentElement.clientWidth;
   let h = document.documentElement.clientHeight;
   if (w <= 600) {
+    navbar.style.width ="0%";
     navbar.classList.add("overlay");
     navbarContent.classList.add("overlay-content");
   } else {
+    navbar.style.width="100%";
     navbar.classList.remove("overlay");
     navbarContent.classList.remove("overlay-content");
+    // closeButton.parentNode.removeChild("a");
   };
-
 }
+
+
 // adds or removes navbar curtain as window resizes
 window.addEventListener("resize", addCurtainOverlay);
 
 // end of init
+}
+function openNav() {
+  navbar.style.width ="100%";
+}
+function closeNav() {
+  navbar.style.width ="0%";
 }
 
 

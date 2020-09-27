3b4862b2a95d55399ac976857c627d5ca7687f90
Leo <leo@nybblebox.com>
2020-09-27 03:01:34 -0700

    Fix bugs for the curtain navbar. open button needs to stick the top corner of the page. need to find the solution

--- cupkays.js
+++ cupkays.js
@@ -6,27 +6,26 @@ function init() {
   let navbar = document.getElementById("navbar");
   let sticky = navbar.offsetTop;
   let navbarContent = document.getElementById("navbar-content");
-  let closeButton =
-  document.getElementById("closeBtn");
+  let openButton = document.getElementById("openButton")
+  let closeButton = document.getElementById("closeButton");
 
 window.onscroll = () => stickyNavbar();
-let stickyNavbar = () => {
+function stickyNavbar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
+    openButton.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
+    openButton.classList.remove("sticky");
   };
 };
-// adds class as soon as the page loads
-navbar.classList.add("overlay");
-navbarContent.classList.add("overlay-content");
-// adds class as soon as the page loads
 
 function addCurtainOverlay() {
   let w = document.documentElement.clientWidth;
   let h = document.documentElement.clientHeight;
+
   if (w <= 600) {
-    navbar.style.width ="0%";
+    navbar.style.width ="0";
     navbar.classList.add("overlay");
     navbarContent.classList.add("overlay-content");
   } else {
@@ -39,19 +38,17 @@ function addCurtainOverlay() {
 
 
 // adds or removes navbar curtain as window resizes
+
+window.addEventListener("onload", addCurtainOverlay());
 window.addEventListener("resize", addCurtainOverlay);
 
 // end of init
 }
+
+
 function openNav() {
   navbar.style.width ="100%";
 }
 function closeNav() {
   navbar.style.width ="0%";
 }
-
-
-
-
-
-//end of onload

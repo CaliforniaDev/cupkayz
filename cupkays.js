27823a3d7633b69dde0a36f6ac5f94b28c14c1bd
Leo <leo@nybblebox.com>
2020-09-30 02:46:11 -0700

    finished navbar design now  creating a grid layout for content.

--- cupkays.js
+++ cupkays.js
@@ -2,14 +2,16 @@
 
 window.onload = init;
 
+
 function init() {
   let navbar = document.getElementById("navbar");
   let sticky = navbar.offsetTop;
   let navbarContent = document.getElementById("navbar-content");
   let openButton = document.getElementById("openButton")
   let closeButton = document.getElementById("closeButton");
-
-window.onscroll = () => stickyNavbar();
+  window.onscroll = () => stickyNavbar();
+  window.addEventListener("onload", addCurtainOverlay());
+  window.addEventListener("resize", addCurtainOverlay);
 function stickyNavbar() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky");
@@ -39,16 +41,26 @@ function addCurtainOverlay() {
 
 // adds or removes navbar curtain as window resizes
 
-window.addEventListener("onload", addCurtainOverlay());
-window.addEventListener("resize", addCurtainOverlay);
 
 // end of init
 }
+// end of init
 
 
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
+
+// Navbar Open-Close //
 function openNav() {
   navbar.style.width ="100%";
 }
 function closeNav() {
   navbar.style.width ="0%";
 }
+//
